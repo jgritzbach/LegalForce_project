29e59e67bbeeaac64b95301c448a7020efa78fd4
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-01-09 22:21:25 +0100

    LegalForce -> is_workday() added; stores holidays in the instance list; switched logic of raising TypeErrors
    Because later on we might need to check for public_holiday in iterative manner, we want to store list of holidays in the instance attribute instead of obtaining it every time with method that creates the list from scratch.
    
    czech_public_holidays() methods was kept, but is now used to set instance holiday attribute in the __init__()
    
    New year holiday of the next year was added to the list of holidays to cover legal force computation at the end of the current year. This solution would not be smart, should the 15-day deadline for appeal ever change or new public holidays were added. In such case, more "dynamic" approach of calculating holidays of the next year should be considered. This is highly unlikely though, so this simple solution will do.
    
    The logic of raising value errors if given_date was not valid was switched - now raising the error comes first and comes return next. This should make the code more readable.
    
    is_workday() method added. Simply, date is a workday when it is not weekend or public holiday.

--- legal_force.py
+++ legal_force.py
@@ -3,30 +3,40 @@ from dateutil import easter
 
 class LegalForce:
 
+    def __init__(self):
+        """
+        On creation, we store czech public holidays in the instance list so that we can access it without calling function again
+        """
+        self.__holidays = self.czech_public_holidays()  # method is used to determine the holidays,
+
     def is_weekend(self, given_date):
         """
-        returns true if given date is saturday or sunday. Otherwise, returns false
+        returns True if given date is saturday or sunday. Otherwise, returns False
         """
-        if self.__valid_date(given_date):
-            return given_date.weekday() in {5, 6}
-        raise TypeError
+        if not self.__valid_date(given_date):
+            raise TypeError
+
+        return given_date.weekday() in {5, 6}
 
     def is_public_holiday(self, given_date):
         """
-        returns true if given date is czech public holiday. Otherwise, returns false
+        returns True if given date is czech public holiday. Otherwise, returns False
         """
-        if self.__valid_date(given_date):
-            return given_date in self.__czech_public_holidays()
-        raise TypeError
+        if not self.__valid_date(given_date):
+            raise TypeError
 
-    def __valid_date(self, date_to_test):
+        return given_date in self.__holidays
+
+    def is_workday(self, given_date):
         """
-        return true if argument is either datetime.date or datetime.datetime object
+        returns True if given date is ordinary workday. Otherwise, returns False
         """
+        if not self.__valid_date(given_date):
+            raise TypeError
 
-        return isinstance(date_to_test,date) or isinstance(date_to_test,datetime)
+        return not (self.is_weekend(given_date) or self.is_public_holiday(given_date))
 
-    def __czech_public_holidays(self, year = None):
+    def czech_public_holidays(self, year = None):
         """returns a list of public holidays in the Czech Republic for a given year
 
         :param year: a year is needed to determine the Easter. Omitting the year results in current year being used.
@@ -51,6 +61,15 @@ class LegalForce:
         date(year, 12, 24),  # Christmas Eve
         date(year, 12, 25),  # First Day of Christmas
         date(year, 12, 26),  # Second Day of Christmas
+        date(year + 1, 1, 1),  # next New year
         ]
 
         return holidays
+
+    def __valid_date(self, date_to_test):
+        """
+        return true if argument is either datetime.date or datetime.datetime object
+        """
+
+        return isinstance(date_to_test,date) or isinstance(date_to_test,datetime)
+
