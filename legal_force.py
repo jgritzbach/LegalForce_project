1b9e561ebc5d89e8ac32209a5c665a7b6677b989
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-01-09 22:26:42 +0100

    LegalForce.czech_public_holidays() -> comparing year argument to None corrected to "is" keyword instead of == operator

--- legal_force.py
+++ legal_force.py
@@ -1,11 +1,12 @@
 from datetime import date, datetime, timedelta
 from dateutil import easter
 
+
 class LegalForce:
 
     def __init__(self):
         """
-        On creation, we store czech public holidays in the instance list so that we can access it without calling function again
+        On creation, we store czech public holidays in the instance list - no need for recreating the list later
         """
         self.__holidays = self.czech_public_holidays()  # method is used to determine the holidays,
 
@@ -36,32 +37,32 @@ class LegalForce:
 
         return not (self.is_weekend(given_date) or self.is_public_holiday(given_date))
 
-    def czech_public_holidays(self, year = None):
+    def czech_public_holidays(self, year=None):
         """returns a list of public holidays in the Czech Republic for a given year
 
         :param year: a year is needed to determine the Easter. Omitting the year results in current year being used.
         :return: a list of datetime.date objects
         """
-        if year == None:
+        if year is None:
             year = date.today().year
 
         easter_date = easter.easter(year)
 
         holidays = [
-        date(year, 1, 1),  # New year
-        easter_date + timedelta(-2),  # Good friday
-        easter_date + timedelta(1),  # Easter monday
-        date(year, 5, 1),  # Labor day
-        date(year, 5, 1),  # Day of liberation of Czechoslovakia
-        date(year, 7 ,5),  # Day of the Slavic Apostles Cyril and Methodius
-        date(year, 7, 6),  # Day of the Burning of Master Jan Hus
-        date(year, 9, 28),  # Day of Czech Statehood
-        date(year, 10, 28),  # Day of the Establishment of the Independent Czechoslovak State
-        date(year, 11, 17),  # Day of Struggle for Freedom and Democracy
-        date(year, 12, 24),  # Christmas Eve
-        date(year, 12, 25),  # First Day of Christmas
-        date(year, 12, 26),  # Second Day of Christmas
-        date(year + 1, 1, 1),  # next New year
+            date(year, 1, 1),  # New year
+            easter_date + timedelta(-2),  # Good friday
+            easter_date + timedelta(1),  # Easter monday
+            date(year, 5, 1),  # Labor day
+            date(year, 5, 1),  # Day of liberation of Czechoslovakia
+            date(year, 7, 5),  # Day of the Slavic Apostles Cyril and Methodius
+            date(year, 7, 6),  # Day of the Burning of Master Jan Hus
+            date(year, 9, 28),  # Day of Czech Statehood
+            date(year, 10, 28),  # Day of the Establishment of the Independent Czechoslovak State
+            date(year, 11, 17),  # Day of Struggle for Freedom and Democracy
+            date(year, 12, 24),  # Christmas Eve
+            date(year, 12, 25),  # First Day of Christmas
+            date(year, 12, 26),  # Second Day of Christmas
+            date(year + 1, 1, 1),  # next New year
         ]
 
         return holidays
@@ -71,5 +72,4 @@ class LegalForce:
         return true if argument is either datetime.date or datetime.datetime object
         """
 
-        return isinstance(date_to_test,date) or isinstance(date_to_test,datetime)
-
+        return isinstance(date_to_test, date) or isinstance(date_to_test, datetime)
