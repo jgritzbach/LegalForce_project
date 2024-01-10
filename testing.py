3609ad86266686fd4cfe27893b0b068a268ad3b3
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-01-10 10:44:18 +0100

    testing.py removed from git tracking.

--- testing.py
+++
@@ -1,9 +0,0 @@
-"""
-This module serves as a playground and a test lab for evolving code
-"""
-
-from legal_force import LegalForce
-import datetime as dt
-
-lf = LegalForce()
-print(lf.is_weekend(dt.date(2024,1,6)))
